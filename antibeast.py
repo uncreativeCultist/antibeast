a467e1a001c829be9dcb88c32d09a306e5b43ebb
uncreativeCultist <66176496+uncreativeCultist@users.noreply.github.com>
2026-07-06 18:55:21 -0400

    this is shockingly good

---
+++ antibeast.py
@@ -0,0 +1,97 @@
+from email.mime import image, text
+import re
+import subprocess
+from urllib.parse import urlparse
+import discord
+from PIL import Image
+import pytesseract
+import datetime
+
+token=""
+server_id=271369861416878081 #anomalous materials discord, this is a placeholder!!!
+
+img_exts = (".png", ".jpg", ".jpeg", ".webp") # if some insane fuck uses a bitmap image to bypass this istg
+def get_image_urls(message):
+    urls = []
+
+    for attachment in message.attachments:
+        urls.append(attachment.url)
+    # fuck this
+    for match in re.findall(r"https?://\S+", message.content):
+        clean_url = match.rstrip(".,;:!?\"")
+        path = urlparse(clean_url).path.lower()
+        if any(path.endswith(ext) for ext in img_exts):
+            urls.append(clean_url)
+
+    return urls
+
+# and thus begins the stolen code (https://discordpy.readthedocs.io/en/stable/quickstart.html)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+#    if message.author == client.user:
+#        return
+    if message.guild.id == server_id:
+        rating=0
+        timedout=0
+        uid = message.author.id
+        for image_url in get_image_urls(message):
+            print(image_url)
+            subprocess.run(["curl", f"{image_url}", "-o", "temp"]) # i dont like this but it works and i'm too lazy to fix. luna, please add details.
+            image = Image.open("temp")
+            gray_image = image.convert("L")
+            text = pytesseract.image_to_string(gray_image)
+            clean_text = text.replace("\x0c", "").strip()
+            nice_text=clean_text.lower()
+            print(nice_text)
+            print()
+            if "mrbeast" in nice_text:
+                rating = rating+1
+            if "joined april" in nice_text:
+                rating = rating+1
+            if "follow me for a" in nice_text:
+                rating = rating+1
+            if "crypto" in nice_text:
+                rating = rating+2
+            if "media personality" in nice_text:
+                rating = rating+1
+            if "kasowin" in nice_text:
+                rating = rating+2
+            if "vyro" in nice_text:
+                rating = rating+1
+            if "gambwex" in nice_text:
+                rating = rating+2
+            if "bonus" in nice_text:
+                rating = rating+1
+            if "reward" in nice_text:
+                rating = rating+1
+
+            if rating > 5:
+                duration = datetime.timedelta(minutes=30) # i dont like deltatime
+                await message.author.timeout(duration, reason="scam image (automatic)")
+                timedout=1
+
+            if rating > 4:
+                if timedout!=1:
+                    duration = datetime.timedelta(minutes=10) # i dont like deltatime
+                    await message.author.timeout(duration, reason="scam image (automatic)")
+                    timedout=1
+
+            if rating > 3:
+                if timedout!=1:
+                    duration = datetime.timedelta(minutes=1) # i dont like deltatime
+                    await message.author.timeout(duration, reason="scam image (automatic)")
+
+            if rating > 2:
+                await message.reply(f"hi there! it looks like you tried to post one of those crypto scam images. if this is incorrect, please contact the mod team <3 \n-# Debug info: sr:{rating}")
+                await message.delete()
+                print("deleted scam link")
+
+            subprocess.run(["rm", "temp"])
+        
+client.run(token)
