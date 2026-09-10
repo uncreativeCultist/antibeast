1ff5aae03fb6982dca2bdc784158ef778f1a544b
Dahlia B. <uncreative@cultist.gay>
2026-09-10 14:23:53 -0400

    bit of an overhaul

--- antibeast.py
+++ antibeast.py
@@ -6,9 +6,14 @@ import discord
 from PIL import Image
 import pytesseract
 import datetime
+import os
+from dotenv import load_dotenv
 
-token=""
-server_id=271369861416878081 #anomalous materials discord, this is a placeholder!!!
+load_dotenv()
+token = os.getenv('ANTIBEAST_TOKEN')
+if token is None:
+    print('You forgot to add the bot token to the "ANTIBEAST_TOKEN" environment variable.')
+    raise SystemExit(1)
 
 img_exts = (".png", ".jpg", ".jpeg", ".webp") # if some insane fuck uses a bitmap image to bypass this istg
 def get_image_urls(message):
@@ -37,68 +42,68 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.guild.id == server_id:
-        rating=0
-        timedout=0
-        uid = message.author.id
-        for image_url in get_image_urls(message):
-            subprocess.run(["curl", f"{image_url}", "-o", "temp"]) # i dont like this but it works and i'm too lazy to fix. luna, please add details.
-            image = Image.open("temp")
-            gray_image = image.convert("L")
-            text = pytesseract.image_to_string(gray_image)
-            clean_text = text.replace("\x0c", "").strip()
-            nice_text=clean_text.lower()
-            if "mrbeast" in nice_text:
-                rating = rating+1
-            if "joined april" in nice_text:
-                rating = rating+1
-            if "follow me for a" in nice_text:
-                rating = rating+1
-            if "beast games" in nice_text:
-                rating = rating+1
-            if "crypto" in nice_text:
-                rating = rating+2
-            if "media personality" in nice_text:
-                rating = rating+1
-            if "kasowin" in nice_text:
-                rating = rating+2
-            if "lacewin" in nice_text:
-                rating = rating+2
-            if "haveawin" in nice_text:
-                rating = rating+2
-            if "vip-club" in nice_text:
-                rating = rating+2
-            if "vyro" in nice_text:
-                rating = rating+1
-            if "gambwex" in nice_text:
-                rating = rating+2
-            if "bonus" in nice_text:
-                rating = rating+1
-            if "withdraw" in nice_text:
-                rating = rating+1
-            if "reward" in nice_text:
-                rating = rating+1
+    rating=0
+    timedout=0
+    uid = message.author.id
+    for image_url in get_image_urls(message):
+        subprocess.run(["curl", f"{image_url}", "-o", f"{uid}"]) # this is a little better ig
+        image = Image.open(f"{uid}")
+        gray_image = image.convert("L")
+        text = pytesseract.image_to_string(gray_image)
+        clean_text = text.replace("\x0c", "").strip()
+        nice_text=clean_text.lower()
+        if "mrbeast" in nice_text:
+            rating = rating+1
+        if "joined april" in nice_text:
+            rating = rating+1
+        if "follow me for a" in nice_text:
+            rating = rating+1
+        if "beast games" in nice_text:
+            rating = rating+1
+        if "crypto" in nice_text:
+            rating = rating+2
+        if "media personality" in nice_text:
+            rating = rating+1
+        if "kasowin" in nice_text:
+            rating = rating+2
+        if "lacewin" in nice_text:
+            rating = rating+2
+        if "haveawin" in nice_text:
+            rating = rating+2
+        if "vip-club" in nice_text:
+            rating = rating+2
+        if "vyro" in nice_text:
+            rating = rating+1
+        if "gambwex" in nice_text:
+            rating = rating+2
+        if "bonus" in nice_text:
+            rating = rating+1
+        if "withdraw" in nice_text:
+            rating = rating+1
+        if "reward" in nice_text:
+            rating = rating+1
 
-            if rating > 5:
-                duration = datetime.timedelta(minutes=30) # i dont like deltatime
-                await message.author.timeout(duration, reason="scam image (automatic)")
+        if rating > 5:
+            duration = datetime.timedelta(minutes=30) # i dont like deltatime
+            await message.author.timeout(duration, reason="MrBeast Scam Bot (AntiBeast)")
+            timedout=1
+
+        if rating > 4:
+            if timedout!=1:
+                duration = datetime.timedelta(minutes=10) # i dont like deltatime
+                await message.author.timeout(duration, reason="MrBeast Scam Bot (AntiBeast)")
                 timedout=1
 
-            if rating > 4:
-                if timedout!=1:
-                    duration = datetime.timedelta(minutes=10) # i dont like deltatime
-                    await message.author.timeout(duration, reason="scam image (automatic)")
-                    timedout=1
+        if rating > 3:
+            if timedout!=1:
+                duration = datetime.timedelta(minutes=1) # i dont like deltatime
+                await message.author.timeout(duration, reason="MrBeast Scam Bot (AntiBeast)")
 
-            if rating > 3:
-                if timedout!=1:
-                    duration = datetime.timedelta(minutes=1) # i dont like deltatime
-                    await message.author.timeout(duration, reason="scam image (automatic)")
+        if rating > 2:
+            user = await client.fetch_user(uid)
+            await user.send(f'Hi there! It looks like you tried to post one of those crypto scam images in "{message.guild}". \nIf this is incorrect, please contact the mod team <3\n\nIf you **did not** attempt to send an image in "{message.guild}" just now, **change your password, your account has been compromised.**')
+            await message.delete()
 
-            if rating > 2:
-                await message.reply(f"hi there! it looks like you tried to post one of those crypto scam images. if this is incorrect, please contact the mod team <3 \n-# Debug info: sr:{rating}")
-                await message.delete()
-
-            subprocess.run(["rm", "temp"])
+        subprocess.run(["rm", f"{uid}"])
         
 client.run(token)
